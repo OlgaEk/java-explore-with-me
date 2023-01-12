81f1e5cc8a420d74edd22af43b8f4e48ac01e555
OlgaEk <sunflower2003@mail.ru>
2023-01-12 14:33:33 +0300

    CheckStyle

--- ewm-main-service/src/main/java/ru/practicum/ewm/user/model/User.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/user/model/User.java
@@ -1,11 +1,9 @@
 package ru.practicum.ewm.user.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Getter
